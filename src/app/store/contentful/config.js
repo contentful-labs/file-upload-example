30d5a0fecbd1de4d5643c829bd775dfb7dd170c4
Khaledgarbaya <khaledgarbaya@gmail.com>
2017-03-07 21:14:37 +0100

    feat(Login): Make host and upload host configurable

--- src/app/store/contentful/config.js
+++ src/app/store/contentful/config.js
@@ -3,5 +3,7 @@ export const name = 'CONTENTFUL'
 export const initialState = {
   accessToken: null,
   spaceId: null,
+  host: 'api.contentful.com',
+  hostUpload: 'upload.contentful.com',
   assets: []
 }
--- src/app/store/contentful/selectors.js
+++ src/app/store/contentful/selectors.js
@@ -1,3 +1,5 @@
 export const selectAccessToken = (state) => state.contentful.accessToken
 export const selectSpaceId = (state) => state.contentful.spaceId
+export const selectHost = (state) => state.contentful.host
+export const selectHostUpload = (state) => state.contentful.hostUpload
 export const selectAssets = (state) => state.contentful.assets
