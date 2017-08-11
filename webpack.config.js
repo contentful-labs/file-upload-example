c3a30afda7d6d40a309dad4b862235d9e204c10a
Benedikt Rötsch <benedikt@contentful.com>
2017-08-11 15:21:09 +0200

    refactor(babel): reduce size via useBuiltIns of preset-env v2

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const __PROD__ = process.env.NODE_ENV === 'production'
 
 const webpackConfig = {
   entry: {
-    app: ['babel-polyfill', join(webpackSource, 'app.js')]
+    app: [join(webpackSource, 'app.js')]
   },
   devtool: __DEV__ ? '#cheap-eval-source-map' : false,
   output: {
